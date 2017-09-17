d8cae3dda2d296f3fafeec68a9cb97d7d2be3298
bomba1990 <marianoramirez353@gmail.com>
2017-09-17 13:12:10 -0400

    arquetitek allowed hosts

--- blog/config/settings/production.py
+++ blog/config/settings/production.py
@@ -11,4 +11,4 @@ COMPRESS_OFFLINE = True
 DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 
-ALLOWED_HOSTS = ['192.168.2.169']
+ALLOWED_HOSTS = ['192.168.2.169',"arquetitek.com"]
