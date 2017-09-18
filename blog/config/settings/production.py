778399f9370b43784ebfda042ef5ecc950a41700
bomba1990 <marianoramirez353@gmail.com>
2017-09-17 21:29:34 -0400

    sentry

--- blog/config/settings/production.py
+++ blog/config/settings/production.py
@@ -1,4 +1,5 @@
 import dj_database_url
+import raven
 
 from .base import *
 
@@ -12,3 +13,14 @@ DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 
 ALLOWED_HOSTS = ['192.168.2.169',"arquetitek.com"]
+
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
+
+RAVEN_CONFIG = {
+    'dsn': 'https://f6a5164b658843d4991a513d395986bc:96c5bb69788d463a835f56a6038a431e@sentry.io/218182',
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.dirname(BASE_DIR)),
+}
