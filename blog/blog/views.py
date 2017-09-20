38912661e18ef24a62477c2ba240d09fa92e64c0
junior <juniorrivasmendoza@gmail.com>
2017-09-20 00:18:09 -0400

    primera subida

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.core.mail import EmailMultiAlternatives
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.views.generic import DetailView
